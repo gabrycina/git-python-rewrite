a2b26fa4e6dababb529cce0abeb74ec9189c1890
marcomit <marcomenegazzi.05@gmail.com>
2024-04-26 01:08:15 +0200

    ls-tree

--- libtft.py
+++ libtft.py
@@ -27,6 +27,10 @@ argsp.add_argument("-t", metavar="type", dest="type", choices=["blob", "commit",
 argsp.add_argument("-w", dest="write", action="store_true", help="Actually write the object into the database")
 argsp.add_argument("path", help="Read object from <file>")
 
+#subparser for ls-tree
+argsp = argsubparsers.add_parser("ls-tree", help="Pretty-print a tree object.")
+argsp.add_argument("-r", dest="recursive", action="store_true", help="Recurse into sub-trees")
+argsp.add_argument("tree", help="A tree-ish object.")
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
@@ -361,6 +365,25 @@ def kvlm_serialize(kvlm):
 
     return res
   
+def ls_tree(repo, ref, recursive=None, prefix=''):
+    obj = object_read(repo, object_find(repo, ref, fmt=b'tree'))
+    for item in obj.items:
+        type = item.mode[0:(1 if len(item.mode) == 5 else 2)]
+        match (type):
+            case '04': type = 'tree'
+            case '10': type = 'blob'
+            case '12': type = 'blob'
+            case '16': type = 'blob'
+            case _: raise Exception("Unknown type %s!" % type)
+        if not (recursive and type == 'tree'):
+            print("{0} {1} {2}\t{3}".format(
+                "0" * (6 - len(item.mode)) + item.mode.decode("ascii"), type,
+                item.sha,
+                os.path.join(prefix, item.path)))
+        else:
+            ls_tree(repo, item.sha, recursive, prefix=os.path.join(prefix, item.path))
+
+
 def cat_file(repo, obj, fmt=None):
     obj = object_read(repo, object_find(repo, obj, fmt=fmt))
     sys.stdout.buffer.write(obj.serialize())
@@ -380,3 +403,8 @@ def cmd_hash_object(args):
     with open(args.path, "rb") as fd:
         sha = object_hash(fd, args.type.encode(), repo)
         print(sha)
+
+def cmd_ls_tree(args):
+    """Bridge function to list the contents of a tree object."""
+    repo = repo_find()
+    ls_tree(repo, args.tree, args.recursive)
